61b4564c1163d1a670113ead659a2dad6ad3ba1f
Louis-Wen <rick40245@gmail.com>
2025-06-05 14:03:56 +0900

    Optimize the layout and style of ConfirmDialog.vue to improve readability and standardize indentation.

--- src/components/ConfirmDialog.vue
+++ src/components/ConfirmDialog.vue
@@ -1,12 +1,18 @@
 <template>
-    <el-dialog v-model:visible="dialogVisible" title="流程中斷警告" width="350px" :close-on-click-modal="false"
-        :close-on-press-escape="false" :show-close="false">
-        <div>你尚未完成預約流程，確定要離開嗎？</div>
-        <template #footer>
-            <el-button @click="onCancel">取消</el-button>
-            <el-button type="danger" @click="onConfirm">確認離開</el-button>
-        </template>
-    </el-dialog>
+  <el-dialog
+    v-model:visible="dialogVisible"
+    title="流程中斷警告"
+    width="350px"
+    :close-on-click-modal="false"
+    :close-on-press-escape="false"
+    :show-close="false"
+  >
+    <div>你尚未完成預約流程，確定要離開嗎？</div>
+    <template #footer>
+      <el-button @click="onCancel">取消</el-button>
+      <el-button type="danger" @click="onConfirm">確認離開</el-button>
+    </template>
+  </el-dialog>
 </template>
 
 <script setup lang="ts">
@@ -15,14 +21,14 @@ const props = defineProps<{ visible: boolean }>()
 const emit = defineEmits(['update:visible', 'confirm', 'cancel'])
 
 const dialogVisible = computed({
-    get: () => props.visible,
-    set: (val: boolean) => emit('update:visible', val)
+  get: () => props.visible,
+  set: (val: boolean) => emit('update:visible', val),
 })
 
 function onConfirm() {
-    emit('confirm')
+  emit('confirm')
 }
 function onCancel() {
-    emit('cancel')
+  emit('cancel')
 }
 </script>
